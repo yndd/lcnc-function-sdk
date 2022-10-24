6111a0ee4b13e549a4cab72cc21f97b22a0927a8
Wim Henderickx <wim.henderickx@gmail.com>
2022-10-24 06:47:13 -0400

    added new json unmarshal error

--- go/fn/resourcecontext.go
+++ go/fn/resourcecontext.go
@@ -2,14 +2,16 @@ package fn
 
 import (
 	"encoding/json"
+	"fmt"
 
 	rctxv1 "github.com/yndd/lcnc-runtime/pkg/api/resourcecontext/v1"
 )
 
 func ParseResourceContext(input []byte) (*rctxv1.ResourceContext, error) {
 	rc := &rctxv1.ResourceContext{}
+	
 	if err := json.Unmarshal(input, rc); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error: %s with bytes: %s", err.Error(), string(input))
 	}
 	return rc, nil
 }
