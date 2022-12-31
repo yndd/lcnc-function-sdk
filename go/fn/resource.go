5a55b307d69e425bd8e90b764b006fe28f816097
Wim Henderickx <wim.henderickx@gmail.com>
2022-12-31 06:18:37 +0100

    added methods to the resoruces to insert unique objects

--- go/fn/resource.go
+++ go/fn/resource.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
@@ -23,6 +24,20 @@ type Resources struct {
 type Object interface {
 	metav1.Object
 	runtime.Object
+}
+
+func (r *Resources) AddIntput(o Object) error {
+	gvkString := GetGVKString(o)
+	_, ok := r.Input[gvkString]
+	if !ok {
+		r.Input[gvkString] = []runtime.RawExtension{}
+	}
+	b, err := json.Marshal(o)
+	if err != nil {
+		return err
+	}
+	r.Input[gvkString] = append(r.Input[gvkString], runtime.RawExtension{Raw: b})
+	return nil
 }
 
 func (r *Resources) AddOutput(o Object) error {
@@ -53,6 +68,67 @@ func (r *Resources) AddCondition(o Object) error {
 	return nil
 }
 
+func (r *Resources) AddUniqueIntput(o Object) error {
+	gvkString := GetGVKString(o)
+	_, ok := r.Input[gvkString]
+	if !ok {
+		r.Input[gvkString] = []runtime.RawExtension{}
+	}
+	b, err := json.Marshal(o)
+	if err != nil {
+		return err
+	}
+	present, idx := isPresent(r.Input[gvkString], o)
+	if !present {
+		r.Input[gvkString] = append(r.Input[gvkString], runtime.RawExtension{Raw: b})
+	} else {
+		// overwrite
+		r.Input[gvkString][idx] = runtime.RawExtension{Raw: b}
+	}
+	return nil
+}
+
+func (r *Resources) AddUniqueOutput(o Object) error {
+	gvkString := GetGVKString(o)
+	_, ok := r.Output[gvkString]
+	if !ok {
+		r.Output[gvkString] = []runtime.RawExtension{}
+	}
+	b, err := json.Marshal(o)
+	if err != nil {
+		return err
+	}
+	present, idx := isPresent(r.Output[gvkString], o)
+	if !present {
+		r.Output[gvkString] = append(r.Output[gvkString], runtime.RawExtension{Raw: b})
+	} else {
+		// overwrite
+		r.Output[gvkString][idx] = runtime.RawExtension{Raw: b}
+	}
+	return nil
+}
+
+func (r *Resources) AddUniqueCondition(o Object) error {
+	gvkString := GetGVKString(o)
+	_, ok := r.Conditions[gvkString]
+	if !ok {
+		r.Conditions[gvkString] = []runtime.RawExtension{}
+	}
+	b, err := json.Marshal(o)
+	if err != nil {
+		return err
+	}
+	present, idx := isPresent(r.Conditions[gvkString], o)
+	if !present {
+		r.Conditions[gvkString] = append(r.Conditions[gvkString], runtime.RawExtension{Raw: b})
+	} else {
+		// overwrite
+		r.Conditions[gvkString][idx] = runtime.RawExtension{Raw: b}
+	}
+	return nil
+}
+
+
 func GetGVKString(o Object) string {
 	gvk := o.GetObjectKind().GroupVersionKind()
 	return GVKToString(&gvk)
@@ -69,4 +145,17 @@ func StringToGVK(s string) *schema.GroupVersionKind {
 		gvk = &schema.GroupVersionKind{Group: s[2], Version: s[1], Kind: s[0]}
 	}
 	return gvk
+}
+
+func isPresent(slice []runtime.RawExtension, o Object) (bool, int) {
+	for idx, v := range slice {
+		u := &unstructured.Unstructured{}
+		if err := json.Unmarshal(v.Raw, u); err != nil {
+			return false, 0
+		}
+		if u.GetName() == o.GetName() && u.GetNamespace() == o.GetNamespace() {
+			return true, idx
+		}
+	}
+	return false, 0
 }
